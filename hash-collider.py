ac6dd30ac78ce660b1948c23d42baced8182ee4e
mgeeky <mgeeky@users.noreply.github.com>
2016-08-10 13:06:40 +0200

    Working towards proper parsers importing and registering functionality

--- hash-collider.py
+++ hash-collider.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python
 
+import os
 import re
 import sys
 import urllib
-import urlparse
 import hashlib
+import itertools
+import multiprocessing
+from myexceptions import *
+from ctypes import c_char_p
+
+try:
+    import parmap
+except ImportError:
+    raise ImportError("Could not import `parmap` module. Try with: pip install parmap")
+
+try:
+    import importlib
+except ImportError:
+    raise ImportError("Could not import `importlib` module. Try with: pip install importlib")
 
 
 DEBUG = True
@@ -22,9 +36,27 @@ def dbg(txt):
     if DEBUG:
         info('[dbg] '+txt)
 
+def import_file(full_path_to_module):
+    # http://stackoverflow.com/a/68628
+    try:
+	import os
+	module_dir, module_file = os.path.split(full_path_to_module)
+	module_name, module_ext = os.path.splitext(module_file)
+	save_cwd = os.getcwd()
+	os.chdir(module_dir)
+	module_obj = __import__(module_name)
+	module_obj.__file__ = full_path_to_module
+	globals()[module_name] = module_obj
+	os.chdir(save_cwd)
+        return module_obj
+    except:
+	raise ImportError
+
+
 class Hasher:
     hashlens = {}
     hashing_algo = None
+    incr_lock = multiprocessing.Lock()
     checks = 0
     
     def __init__(self, data):
@@ -51,51 +83,17 @@ class Hasher:
         if not self.hashing_algo:
             for a, l in self.hashlens.items():
                 if l == len(self.data):
-                    self.hashing_algo = hashlib.__get_builtin_constructor(a)
+                    with self.incr_lock:
+                        self.hashing_algo = hashlib.__get_builtin_constructor(a)
         return self.hashing_algo
 
     def hashit(self, data):
         return self.get_algo()(data).hexdigest()
 
     def check(self, data):
-        self.checks += 1
+        with self.incr_lock:
+            self.checks += 1
         return self.data == self.hashit(data)
-
-class HttpRequestParser:
-    name = 'HttpRequestParser'
-
-    def check(self, data):
-        return False
-
-    def parse(self, data):
-        return None
-
-class DateTimestampParser:
-    name = 'HttpRequestParser'
-
-    def check(self, data):
-        return False
-
-    def parse(self, data):
-        return None
-
-
-class HttpParamsParser:
-    name = 'HttpParamsParser'
-
-    def check(self, data):
-        try:
-            self.parse(data)
-            return True
-        except ValueError:
-            return False
-
-    def parse(self, data):
-        vals = urlparse.parse_qsl(data, True, True)
-        out = {}
-        for val in vals:
-            out[val[0]] = val[1]
-        return out
 
 
 class HashCollider:
@@ -105,19 +103,42 @@ class HashCollider:
 
     def __init__(self, data):
         hasher = Hasher(data)
+        self.register_parsers()
 
-    def register_parser(self, parser):
-        if hasattr(parser, 'check'):
-            if hasattr(parser, 'parse'):
-                if hasattr(parser, 'name'):
-                    dbg("Registering new parser: %s" % parser.name)
-                    self.parsers.append(parser)
+    def load_parser(self, parser):
+        dbg("Loading parser: '%s'" % parser)
+        mod = import_file(parser)
+        for objs in mod.__dict__.keys():
+            if 'Parser' in objs:
+                dbg("Found parser's definition: %s" % objs)
+
+
+    def register_parsers(self, parsers=None):
+        def _register_parser(parser):
+            if hasattr(parser, 'check'):
+                if hasattr(parser, 'parse'):
+                    if hasattr(parser, 'name'):
+                        dbg("Registering new parser: %s" % parser.name)
+                        self.parsers.append(parser)
+                    else:
+                        raise InvalidParserException, "Parser registration failed: no `name` attribute found"
                 else:
-                    assert False, "Parser registration failed: no `name` attribute found"
+                    raise InvalidParserException, "Parser registration failed: no `parse()` method found"
             else:
-                assert False, "Parser registration failed: no `parse()` method found"
+                raise InvalidParserException, "Parser registration failed: no `check()` method found"
+
+        if parsers == None:
+            # Registering all of the available parsers.
+            currpath = os.path.realpath(__file__)
+            modules_path = os.path.join(currpath, 'parsers')
+            for top, _, parsers in os.walk(modules_path):
+                map(self.load_parser, map(lambda x: os.path.join(top, x), parsers))
+
         else:
-            assert False, "Parser registration failed: no `check()` method found"
+            if isinstance(parsers, list):
+                map(_register_parser, parsers)
+            else:
+                _register_parser(parsers)
 
     def print_elements(self):
         out = ''
@@ -125,14 +146,19 @@ class HashCollider:
             out += '"{}", '.format(e)
         return out
 
-    def feed(self, element):
-        parsed = self.parse(element)
+    def feed(self, element, parser=None):
+        try:
+            parsed = self.parse(element, parser)
+        except ParserNotFoundException:
+            warning("Could not parse input data.")
+            return False
+            
         l1 = len(self.elements)
 
-        if isinstance(parsed, list):
+        if isinstance(parsed, list) or isinstance(parsed, tuple):
             self.elements.update(parsed)
-        elif isinstance(parsed, basestring):
-            self.elements.add(parsed)
+        elif isinstance(parsed, basestring) or isinstance(parsed, int) or isinstance(parsed, float):
+            self.elements.add(str(parsed))
         elif isinstance(parsed, dict):
             for k, v in parsed.items():
                 self.elements.add(k)
@@ -143,12 +169,64 @@ class HashCollider:
         dbg("Fed collider with %d new elements" % (len(self.elements) - l1))
         dbg("\n--- Elements list:\n\t%s\n---\n" % self.print_elements())
 
+        return True
 
-    def parse(self, data):
-        for parser in self.parsers:
-            if parser.check(data):
-                dbg("Parser %s agreed to process: ['%s']..." % (parser.name, data[:64]))
-                return parser.parse(data)
+
+    def parse(self, data, parser1=None):
+        if not parser1:
+            for parser in self.parsers:
+                if parser.check(data):
+                    dbg("Parser %s agreed to process: ['%s']..." % (parser.name, data[:64]))
+                    return parser.parse(data)
+
+            raise ParserNotFoundException
+        else:
+            return parser1.parse(data)
+
+    def generate_combinations(self):
+        def combinations(items):
+            # http://stackoverflow.com/a/6542458
+            return ( set(itertools.compress(items, mask)) for mask in itertools.product(*[[0,1]]*len(items)))
+        
+        elements = set()
+        separators = ['', '+', '|', '.', '-']
+        for comb in combinations(self.elements):
+            for sep in separators:
+                elements.add(sep.join([str(c) for c in comb]))
+
+        return elements
+
+    @staticmethod
+    def _worker(inputs, stopevent, result):
+        for inp in inputs:
+            if stopevent.is_set():
+                return 
+            if self.hasher.check(inp):
+                result.value = inp
+                stopevent.set()
+                return
+
+    def print_result(self, data):
+        return '%s("%s") == "%s"' % (self.hasher.hashing_algo().name, data, self.data)
+
+    def collide(self):
+        dbg("Generating about %d combinations of %d elements" % (2**(len(self.elements)), len(self.elements)))
+        elements = self.generate_combinations()
+
+        workers = min(len(elements), multiprocessing.cpu_count() * 4)
+        dbg("Engaging hashing loop over %d candidates with %d workers. Stay tight." % (len(elements), workers))
+
+        error("Not implemented yet")
+        return False
+
+        #stopevent = multiprocessing.Event()
+        #result = multiprocessing.Value(c_char_p, '')
+        #parmap.map(HashCollider._worker, elements, stopevent, result, processes=workers)
+
+        #info("[+] Got it: %s" % self.print_result(result.value))    
+
+        #return result.value
+        
 
 
 def main():
@@ -161,10 +239,10 @@ def main():
 
     data = sys.argv[1]
     collider = HashCollider(data)
-    collider.register_parser(HttpParamsParser())
 
     d = raw_input("Data: ")
     collider.feed(d)
+    collider.collide()
 
 if __name__ == '__main__':
     main()
